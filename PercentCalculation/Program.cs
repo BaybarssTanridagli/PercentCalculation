01f1aceccd849c8a532abb9e4283379ff01498b7
Baybars <baybarsstanridagli@gmail.com>
2025-01-04 13:44:14 +0300

    PercentCalculation

---
+++ PercentCalculation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PercentCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number, percent, result;
+            Console.WriteLine("Please Enter The Number: ");
+            number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please Enter The Percent: ");
+            percent = Convert.ToDouble(Console.ReadLine());
+            result = number * percent / 100;
+            Console.WriteLine("Number Entered % {0} : {1}",percent,result);
+            Console.ReadKey();
+        }
+    }
+}
